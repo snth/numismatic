9281439a3e6e19ef569ec5eaf249478ed7a07c8f
Tobias Brandt <Tobias.Brandt@gmail.com>
2017-10-07 15:38:51 +0200

    Fixes setup for pypi upload

--- setup.py
+++ setup.py
@@ -3,26 +3,25 @@
 from pathlib import Path
 from setuptools import setup
 
-#README = Path('README.markdown').read()
-README=''
-REQUIREMENTS = list(open(Path('requirements.txt')).read().strip().split('\n'))
+README = Path('README.md').open('rt').read()
+REQUIREMENTS = Path('requirements.txt').open('rt').read().strip().split('\n')
 print(REQUIREMENTS)
 VERSION = open(Path('VERSION')).read().strip()
 print(VERSION)
 
 setup(name='numismatic',
       version=VERSION,
-      description='Coin Collector for digital currencies ',
-      url='http://gitlab.com/crypto-currencies/coin/',
+      description='Coin Collector for digital assets ',
+      url='http://github.com/snth/numismatic/',
       maintainer='Tobias Brandt',
       maintainer_email='Tobias.Brandt@gmail.com',
-      license='Apache',
+      license='MIT',
       keywords='bitcoin,ethereum,cryptocurrencies',
       packages=['numismatic'],
       long_description=README,
       install_requires=REQUIREMENTS,
       python_requires='>=3.6',
-      zip_safe=False,
+      zip_safe=True,
       entry_points='''
           [console_scripts]
           coin=numismatic.cli:coin
