fd9784f59ac717d6435c719b0010c63adeca9659
Tobias Brandt <Tobias.Brandt@gmail.com>
2017-10-14 15:23:34 +0200

    Adds streaming of OrderBook events to LunoExchange

--- numismatic/events.py
+++ numismatic/events.py
@@ -26,3 +26,19 @@ class Trade(Event):
     price = attr.ib()
     volume = attr.ib()
     id = attr.ib(default=None)
+
+@attr.s(slots=True)
+class LimitOrder(Event):
+    exchange = attr.ib()
+    symbol = attr.ib()
+    timestamp = attr.ib()
+    price = attr.ib()
+    volume = attr.ib()
+    id = attr.ib()
+
+@attr.s(slots=True)
+class CancelOrder(Event):
+    exchange = attr.ib()
+    symbol = attr.ib()
+    timestamp = attr.ib()
+    id = attr.ib()
