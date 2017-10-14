f298dde822a2512f69b48a4cbe2048f23be34b68
Tobias Brandt <Tobias.Brandt@gmail.com>
2017-10-14 16:56:25 +0200

    Fixes PyPI upload error by changing classifiers to be a list

--- setup.py
+++ setup.py
@@ -27,11 +27,11 @@ setup(name='numismatic',
           [console_scripts]
           coin=numismatic.cli:coin
           ''',
-      classifiers=(
+      classifiers=[
           'Development Status :: 3 - Alpha',
           'License :: OSI Approved :: MIT License',
           'Programming Language :: Python',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: Implementation :: CPython',
-      ),
+          ],
       )
