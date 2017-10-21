b339754508b49c6bc18f4fb52fc0dea1d93d519a
Tobias Brandt <Tobias.Brandt@gmail.com>
2017-10-21 17:47:00 +0000

    Adds PriorityQueue.peek()

--- numismatic/libs/queue.py
+++ numismatic/libs/queue.py
@@ -2,10 +2,12 @@ from heapq import heappush, heappop
 from itertools import count
 
 class PriorityQueue:
+    # Adapted from:
+    # https://stackoverflow.com/questions/407734/a-generic-priority-queue-for-python
     _REMOVED = object()              # placeholder for a removed item
 
     def __init__(self):
-        self._pq = []                       # heap of entries
+        self._heap = []                       # heap of entries
         self._entry_finder = {}             # mapping of items to entries
         self._counter = count()   # unique sequence count
 
@@ -16,7 +18,7 @@ class PriorityQueue:
         count = next(self._counter)
         entry = [priority, count, item]
         self._entry_finder[item] = entry
-        heappush(self._pq, entry)
+        heappush(self._heap, entry)
 
     def remove(self, item):
         'Mark an existing item as REMOVED.  Raise KeyError if not found.'
@@ -25,13 +27,20 @@ class PriorityQueue:
 
     def pop(self):
         'Remove and return the lowest priority item. Raise KeyError if empty.'
-        while self._pq:
-            priority, count, item = heappop(self._pq)
-            if item is not self._REMOVED:
-                del self._entry_finder[item]
-                return item
-        raise KeyError('pop from an empty priority queue')
+        if not self.empty():
+            priority, count, item = heappop(self._heap)
+            return item
+        else:
+            raise KeyError('pop from an empty priority queue')
 
+    def peek(self):
+        return self._heap[0][2] if not self.empty() else \
+            KeyError('priority queue is empty')
+
+    def empty(self):
+        while self._heap and self._heap[0][2] is self._REMOVED:
+            heappop(self._heap)
+        return not self._heap
 
 
 if __name__=='__main__':
@@ -39,6 +48,10 @@ if __name__=='__main__':
     pq.add('b', 2)
     pq.add('a', 1)
     pq.add('c', 3)
+    pq.add('d', 4)
+    pq.add('e', 5)
+    pq.remove('b')
+    pq.remove('d')
     print(pq.pop())
     print(pq.pop())
     print(pq.pop())
