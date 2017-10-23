e78a838959aa70de45c35a6a4f29b7e4ac1c60b4
Barry Steyn <barry.steyn@gmail.com>
2017-10-22 18:24:22 -0700

    DRY'd code in config methods

--- numismatic/libs/config.py
+++ numismatic/libs/config.py
@@ -2,7 +2,8 @@ from pathlib import Path
 import os
 from configparser import ConfigParser
 
-def get_config():
+def get_all_config():
+    """Returns entire dictionary config"""
     for config_file in [Path(os.environ['HOME']) / '.coinrc']:
       config = ConfigParser()
       if config_file.exists():
@@ -10,3 +11,11 @@ def get_config():
           return config
 
       return None
+
+def get_config(entity_filter=None):
+    """Returns config dictionary with key equal to entity_filer"""
+    config = get_all_config()
+    if (entity_filter is None):
+        return config
+
+    return (config[entity_filter] if entity_filter in config else {})
