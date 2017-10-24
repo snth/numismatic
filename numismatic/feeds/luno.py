d48ed01239debe1a6629d94479ef98dd73f1b6e8
Tobias Brandt <Tobias.Brandt@gmail.com>
2017-10-24 09:35:58 +0200

    Factors out the LunoRestApi

--- numismatic/feeds/luno.py
+++ numismatic/feeds/luno.py
@@ -1,27 +1,38 @@
 from itertools import product
+import logging
 
-from .base import Feed
+from .base import Feed, RestApi
 
 
-class LunoFeed(Feed):
+logger = logging.getLogger(__name__)
+
+
+class LunoRestApi(RestApi):
 
     api_url = 'https://api.mybitx.com/api/1/'
 
-    def get_list(self):
+    def get_tickers(self):
         api_url = f'{self.api_url}/tickers'
         data = self._make_request(api_url)
-        tickers = data['tickers']
+        return data['tickers']
+
+class LunoFeed(Feed):
+
+    def __init__(self, requester='basic', cache_dir=None):
+        self.rest_api = LunoRestApi(requester=requester, cache_dir=cache_dir)
+        self.websocket_api = None
+
+    def get_list(self):
+        tickers = self.rest_api.get_tickers()
         return [ticker['pair'] for ticker in tickers]
 
     def get_info(self, assets):
         raise NotImplementedError('Not available for this feed.') 
 
     def get_prices(self, assets, currencies):
-        api_url = f'{self.api_url}/tickers'
-        data = self._make_request(api_url)
-        tickers = data['tickers']
         assets = assets.upper().split(',')
         currencies = currencies.upper().split(',')
+        tickers = self.rest_api.get_tickers()
         pairs = {f'{asset}{currency}' for asset, currency in 
                  product(assets, currencies)}
         return [ticker for ticker in tickers if ticker['pair'] in pairs]
