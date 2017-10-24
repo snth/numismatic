03c5fffe562289442f096149c59b21b4ae20d94f
Tobias Brandt <Tobias.Brandt@gmail.com>
2017-10-24 09:46:34 +0200

    Factors out BraveNewCoinRestApi

--- numismatic/feeds/bravenewcoin.py
+++ numismatic/feeds/bravenewcoin.py
@@ -1,9 +1,13 @@
-from .base import Feed
+import logging
 
+from .base import Feed, RestApi
 from ..libs.config import get_config
 
 
-class BraveNewCoinFeed(Feed):
+logger = logging.getLogger(__name__)
+
+
+class BraveNewCoinRestApi(RestApi):
 
     api_url = 'https://bravenewcoin-v1.p.mashape.com/'
 
@@ -18,15 +22,33 @@ class BraveNewCoinFeed(Feed):
 
         self.headers = {'X-Mashape-Key': f'{self.api_key_secret}'}
 
-    def get_list(self): 
+    def get_digital_currency_symbols(self): 
         api_url = f'{self.api_url}/digital-currency-symbols'
         data = self._make_request(api_url, headers=self.headers)
-        response = []
+        return data['digital_currencies']
 
+    def get_ticker(self, coin, show):
+        api_url = f'{self.api_url}/ticker/'
+        params = dict(coin=coin, show=show)
+        data = self._make_request(api_url, params=params,
+                                    headers=self.headers)
+        return data if data['success'] else None
+
+
+class BraveNewCoinFeed(Feed):
+
+    def __init__(self, requester='basic', cache_dir=None, api_key_id=None,
+                 api_key_secret=None):
+        self.rest_api = BraveNewCoinRestApi(
+            requester=requester, cache_dir=cache_dir, api_key_id=api_key_id,
+            api_key_secret=api_key_secret)
+        self.websocket_api = None
+
+    def get_list(self): 
+        digital_currencies = self.rest_api.get_digital_currency_symbols()
         response = [key 
-                    for json_dict in data['digital_currencies']
+                    for json_dict in digital_currencies
                     for key, value in json_dict.items()]
-
         return response
 
     def get_info(self, assets):
@@ -39,14 +61,7 @@ class BraveNewCoinFeed(Feed):
         response = []
         for asset in assets.split(','):
             for currency in currencies.split(','):
-                api_url = f'{self.api_url}/ticker/'
-                params = dict(coin=asset, show=currency)
-                data = self._make_request(api_url, params=params,
-                                          headers=self.headers)
-                if not data['success']:
-                    continue
-
+                data = self.rest_api.get_ticker(coin=asset, show=currency)
                 response.append({'asset':asset, 'currency':currency, 
                                  'price': float(data['last_price'])})
-            
         return response
